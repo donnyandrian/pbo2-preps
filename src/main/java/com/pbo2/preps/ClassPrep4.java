2962c9786010f023dfbd2bafde19689d1dc2f7a8
xellanix <107340353+xellanix@users.noreply.github.com>
2025-03-24 19:09:27 +0700

    Output formatting and bug fixed

--- src/main/java/com/pbo2/preps/ClassPrep4.java
+++ src/main/java/com/pbo2/preps/ClassPrep4.java
@@ -1,65 +1,64 @@
 package com.pbo2.preps;
 
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.io.*;
+import java.text.*;
+import java.time.*;
+import java.time.format.*;
+import java.util.*;
 
 /**
- * Represents a product with attributes such as invoice number, stock code, description, quantity, invoice date, unit price, customer ID, and country.
+ * Represents a product with attributes such as invoice number, stock code,
+ * description, quantity, invoice date, unit price, customer ID, and country.
  */
 class Product {
-    private String invoiceNo;
+    private List<String> invoiceNo = new ArrayList<>();
     private String stockCode;
     private String description;
-    private int quantity;
-    private LocalDateTime invoiceDate;
-    private double unitPrice;
-    private int customerID;
-    private String country;
-
-    /**
-     * Default constructor 
-     */
-    public Product() {
-    };
+    private List<Integer> quantity = new ArrayList<>();
+    private List<LocalDateTime> invoiceDate = new ArrayList<>();
+    private List<Double> unitPrice = new ArrayList<>();
+    private List<Integer> customerID = new ArrayList<>();
+    private List<String> country = new ArrayList<>();
 
     /**
      * Parameterized constructor for Product class
-     * @param invoiceNo Invoice number
-     * @param stockCode Stock code
+     * 
+     * @param invoiceNo   Invoice number
+     * @param stockCode   Stock code
      * @param description Product description
-     * @param quantity Quantity of product
+     * @param quantity    Quantity of product
      * @param invoiceDate Date and time of invoice
-     * @param unitPrice Price per unit
-     * @param customerID Customer ID
-     * @param country Country of customer
+     * @param unitPrice   Price per unit
+     * @param customerID  Customer ID
+     * @param country     Country of customer
      */
     public Product(String invoiceNo, String stockCode, String description, int quantity, LocalDateTime invoiceDate,
             double unitPrice, int customerID, String country) {
-        this.invoiceNo = invoiceNo;
+        this.invoiceNo.add(invoiceNo);
         this.stockCode = stockCode;
         this.description = description;
-        this.quantity = quantity;
-        this.invoiceDate = invoiceDate;
-        this.unitPrice = unitPrice;
-        this.customerID = customerID;
-        this.country = country;
+        this.quantity.add(quantity);
+        this.invoiceDate.add(invoiceDate);
+        this.unitPrice.add(unitPrice);
+        this.customerID.add(customerID);
+        this.country.add(country);
+    }
+
+    public Product(Product other) {
+        this.invoiceNo = new ArrayList<>(other.invoiceNo);
+        this.stockCode = other.stockCode;
+        this.description = other.description;
+        this.quantity = new ArrayList<>(other.quantity);
+        this.invoiceDate = new ArrayList<>(other.invoiceDate);
+        this.unitPrice = new ArrayList<>(other.unitPrice);
+        this.customerID = new ArrayList<>(other.customerID);
+        this.country = new ArrayList<>(other.country);
     }
 
     /**
      * Getters and Setters for product attributes.
      */
-    public String getInvoiceNo() {
+    public List<String> getInvoiceNo() {
         return invoiceNo;
     }
 
@@ -71,27 +70,27 @@ class Product {
         return description;
     }
 
-    public int getQuantity() {
+    public List<Integer> getQuantity() {
         return quantity;
     }
 
-    public LocalDateTime getInvoiceDate() {
+    public List<LocalDateTime> getInvoiceDate() {
         return invoiceDate;
     }
 
-    public double getUnitPrice() {
+    public List<Double> getUnitPrice() {
         return unitPrice;
     }
 
-    public int getCustomerID() {
+    public List<Integer> getCustomerID() {
         return customerID;
     }
 
-    public String getCountry() {
+    public List<String> getCountry() {
         return country;
     }
 
-    public void setInvoiceNo(String invoiceNo) {
+    public void setInvoiceNo(List<String> invoiceNo) {
         this.invoiceNo = invoiceNo;
     }
 
@@ -103,55 +102,53 @@ class Product {
         this.description = description;
     }
 
-    public void setQuantity(int quantity) {
+    public void setQuantity(List<Integer> quantity) {
         this.quantity = quantity;
     }
 
-    public void setInvoiceDate(LocalDateTime invoiceDate) {
+    public void setInvoiceDate(List<LocalDateTime> invoiceDate) {
         this.invoiceDate = invoiceDate;
     }
 
-    public void setUnitPrice(double unitPrice) {
+    public void setUnitPrice(List<Double> unitPrice) {
         this.unitPrice = unitPrice;
     }
 
-    public void setCustomerID(int customerID) {
+    public void setCustomerID(List<Integer> customerID) {
         this.customerID = customerID;
     }
 
-    public void setCountry(String country) {
+    public void setCountry(List<String> country) {
         this.country = country;
     }
 
     /**
      * Overridden toString method to display product information in tabular format
+     * 
      * @return Formatted string representation of Product
      */
     @Override
     public String toString() {
-        return String.format("| %-7s | %-12s | %-36s | %-6s | %-16s | %-9s | %-10s | %-20s |",
-                invoiceNo, stockCode, description, quantity,
-                invoiceDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
-                unitPrice, customerID, country);
+        String temp = "";
+
+        NumberFormat nf = NumberFormat.getInstance(Locale.US);
+
+        for (int i = 0; i < invoiceNo.size(); i++) {
+            temp += String.format("| %-7s | %-12s | %-36s | %-6s | %-16s | %-9s | %-10s | %-20s |",
+                    invoiceNo.get(i), stockCode, description, quantity.get(i),
+                    invoiceDate.get(i).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")),
+                    nf.format(unitPrice.get(i)), customerID.get(i), country.get(i));
+
+            if ((i + 1) != invoiceNo.size())
+                temp += "\n";
+        }
+
+        return temp;
     }
 }
 
 // Controller class for managing product operations
 class ProductController {
-    
-    /*
-     * - `List<Product>` untuk menyimpan daftar produk ke dalam bentuk list
-     * - `Set<String>` untuk mendapatkan daftar unik negara pelanggan
-     * - `Map<String, Integer>` untuk menghitung total produk yang terjual
-     * berdasarkan `StockCode`
-     * - `Map<String, Double>` untuk menghitung total pendapatan per negara
-     * - `Map<String, Product>` untuk mencari produk berdasarkan `StockCode`
-     */
-
-    /**
-     * Loads product data from a CSV file.
-     * @param filename The name of the CSV file.
-     */
     private List<Product> products = new ArrayList<>();
     private Map<String, Integer> totalProductsSold = new HashMap<>();
     private Map<String, Double> totalRevenue = new HashMap<>();
@@ -166,6 +163,7 @@ class ProductController {
 
     /**
      * Loads product data from a CSV file.
+     * 
      * @param filename Name of the CSV file.
      */
     public void LoadFromCSV(String filename) {
@@ -174,23 +172,36 @@ class ProductController {
             String line;
             br.readLine(); // skip header
             while ((line = br.readLine()) != null) {
-                Product product = (ParseCSVLine(line));
-                if(product != null){
-                products.add(product);
-                uniqueCountries.add(product.getCountry());
-                productsMap.put(product.getStockCode(), product);
+                Product product = ParseCSVLine(line);
+                if (product != null) {
+                    products.add(product);
+                    uniqueCountries.addAll(product.getCountry());
+
+                    Product existingProduct = null;
+                    if (productsMap.containsKey(product.getStockCode())) {
+                        existingProduct = new Product(productsMap.get(product.getStockCode()));
+
+                        existingProduct.getInvoiceNo().addAll(product.getInvoiceNo());
+                        existingProduct.getQuantity().addAll(product.getQuantity());
+                        existingProduct.getInvoiceDate().addAll(product.getInvoiceDate());
+                        existingProduct.getUnitPrice().addAll(product.getUnitPrice());
+                        existingProduct.getCustomerID().addAll(product.getCustomerID());
+                        existingProduct.getCountry().addAll(product.getCountry());
+                    } else {
+                        existingProduct = new Product(product);
+                    }
+
+                    productsMap.put(product.getStockCode(), existingProduct);
                 }
             }
         } catch (IOException e) {
             System.err.println("Error reading online_retail.csv");
         }
-
-        PrintProductsTable(products);
-        PrintUniqueCountries();
     }
 
     /**
      * Prints the list of products in a table format.
+     * 
      * @param list The list of products to be printed.
      */
     public void PrintProductsTable(List<Product> list) {
@@ -204,17 +215,15 @@ class ProductController {
                         "Country"));
         System.out.println(
                 "+---------+--------------+--------------------------------------+--------+------------------+-----------+------------+----------------------+");
-        int current = 0;
+
         for (Product product : list) {
             System.out.println(product.toString());
-            if (++current == 10) {
-                break;
-            }
         }
 
         System.out.println(
                 "+---------+--------------+--------------------------------------+--------+------------------+-----------+------------+----------------------+");
     }
+
     /**
      * Method to print the products table using the available product list.
      * Calls the PrintProductsTable method with the list of products as a parameter.
@@ -233,12 +242,13 @@ class ProductController {
 
     /**
      * Searches for a product based on StockCode.
+     * 
      * @param stockCode The stock code of the product.
      * @return Product object if found, null otherwise.
      */
-    public Product searchProduct(String stockCode) {
+    public Product SearchProduct(String stockCode) {
         if (!productsMap.containsKey(stockCode)) {
-            System.out.println("Produk with StockCode '" + stockCode + "' not found!");
+            System.out.println("Product with StockCode '" + stockCode + "' not found!");
             return null;
         }
         return productsMap.get(stockCode);
@@ -246,6 +256,7 @@ class ProductController {
 
     /**
      * Parses a single CSV line into a Product object.
+     * 
      * @param line The CSV line.
      * @return A Product object.
      */
@@ -285,8 +296,10 @@ class ProductController {
      */
     public void CountTotalProductsSold() {
         for (Product product : products) {
-            totalProductsSold.put(product.getStockCode(),
-                    totalProductsSold.getOrDefault(product.getStockCode(), 0) + product.getQuantity());
+            String c = product.getStockCode();
+            int v = product.getQuantity().get(0);
+
+            totalProductsSold.put(c, totalProductsSold.getOrDefault(c, 0) + v);
         }
     }
 
@@ -295,8 +308,10 @@ class ProductController {
      */
     public void CountTotalRevenue() {
         for (Product product : products) {
-            Double a = product.getQuantity() * product.getUnitPrice();
-            totalRevenue.put(product.getCountry(), totalRevenue.getOrDefault(product.getCountry(), 0.0) + a);
+            String c = product.getCountry().get(0);
+            Double v = product.getQuantity().get(0) * product.getUnitPrice().get(0);
+
+            totalRevenue.put(c, totalRevenue.getOrDefault(c, 0.0) + v);
         }
     }
 
@@ -307,16 +322,23 @@ class ProductController {
         CountTotalProductsSold();
         CountTotalRevenue();
 
+        NumberFormat nf = NumberFormat.getInstance(Locale.US);
+
         System.out.println("\n======== TOTAL PRODUCTS SOLD TABLE ========");
         System.out.println("+------------------+----------------------+");
         System.out.println(String.format("| %-16s | %-20s |", "StockCode", "Total Products Sold"));
         System.out.println("+------------------+----------------------+");
+        totalProductsSold.forEach(
+                (StockCode, total) -> System.out
+                        .println(String.format("| %-16s | %-20s |", StockCode, nf.format(total))));
         System.out.println("+------------------+----------------------+");
+
         System.out.println("\n============= TOTAL REVENUE TABLE =============");
         System.out.println("+-----------------------+---------------------+");
         System.out.println(String.format("| %-21s | %-19s |", "Country", "Total Revenue"));
         System.out.println("+-----------------------+---------------------+");
-        totalRevenue.forEach((Country, a) -> System.out.println(String.format("| %-21s | %-19.2f |", Country, a)));
+        totalRevenue.forEach(
+                (Country, a) -> System.out.println(String.format("| %-21s | %-19s |", Country, nf.format(a))));
         System.out.println("+-----------------------+---------------------+");
     }
 }
@@ -330,18 +352,21 @@ public class ClassPrep4 {
             Boolean first = true;
             do {
                 if (!first)
-                    System.out.println("\n\n=============================================================================================================================================\n\n");
+                    System.out.println(
+                            "\n\n=============================================================================================================================================\n\n");
 
                 first = false;
-                
+
                 System.out.println("+----------------------------------------------+");
                 System.out.println("|      Welcome To Sales Management System      |");
                 System.out.println("+----------------------------------------------+");
                 System.out.println("|            Please choose an option           |");
                 System.out.println("|                                              |");
                 System.out.println("| 1. Print Products Table                      |");
-                System.out.println("| 2. Generate Business Report                  |");
-                System.out.println("| 3. Exit                                      |");
+                System.out.println("| 2. Print Unique Countries                    |");
+                System.out.println("| 3. Search Product by StockCode               |");
+                System.out.println("| 4. Generate Business Report                  |");
+                System.out.println("| 5. Exit                                      |");
                 System.out.println("|                                              |");
                 System.out.println("+----------------------------------------------+");
                 System.out.print("Enter your choice: ");
@@ -352,21 +377,26 @@ public class ClassPrep4 {
                 if (choice == 1) {
                     controller.PrintProductsTable();
                 } else if (choice == 2) {
-                    controller.GenerateBusinessReport();
+                    controller.PrintUniqueCountries();
                 } else if (choice == 3) {
+                    System.out.print("Enter StockCode of a product to search: ");
+                    sc.nextLine();
+                    String stockCode = sc.nextLine();
+                    Product result = controller.SearchProduct(stockCode);
+
+                    if (result != null) {
+                        System.out.print("\nProduct found!");
+                        controller.PrintProductsTable(Arrays.asList(result));
+                    }
+                } else if (choice == 4) {
+                    controller.GenerateBusinessReport();
+                } else if (choice == 5) {
                     System.out.println("Goodbye!");
                     break;
                 } else {
                     System.out.println("Invalid choice. Please try again.");
                 }
-        } while (true);
-
-        System.out.print("Enter StockCode to search for a product: ");
-        String stockCode = sc.nextLine();
-        Product result = controller.searchProduct(stockCode);
-
-        if (result != null) System.out.println("\nProduct found:\n" + result);
-
-    }
+            } while (true);
+        }
     }
 }
