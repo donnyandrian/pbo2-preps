9e9448d3f14030d201fb8266f071ea2e6c9d6c52
xellanix <107340353+xellanix@users.noreply.github.com>
2025-04-02 20:37:19 +0700

    P5

---
+++ classPrep5/src/main/java/com/pbo2/classPrep5/video2/Counter.java
@@ -0,0 +1,13 @@
+package com.pbo2.classPrep5.video2;
+
+public class Counter {
+    private Long value = 0L;
+
+    public void increment() {
+        value++;
+    }
+
+    public Long getValue() {
+        return value;
+    }
+}
